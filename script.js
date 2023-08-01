a8e67ba5a752fc4da01fd488df30f68c0b3a0329
saabismi <vilho.syvahuoko@gmail.com>
2023-08-01 22:15:42 +0300

    decently working finally
    the form still needs improvements: (automatically current date, automatically select 95E10?, save petrol station names?)
    
    the table uses the wrong date format, location is shown in total cost, total cost isn't calculated and average consumption is empty...

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Set the current date in the date field
     const dateInput = document.getElementById("date");
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`;
-    dateInput.value = formattedDate;
-  
-    // Add event listener to the submit button
     const submitButton = document.getElementById("submit-button");
+  
+    // Handle form submission when clicking the submit button
     submitButton.addEventListener("click", function (event) {
       event.preventDefault();
-      // Implement the logic to handle form submission here
-      // For simplicity, we'll just add a new row to the table with the form data
-      const tableBody = document.querySelector("#fuel-log-table tbody");
-      const newRow = tableBody.insertRow();
-      const inputFields = document.querySelectorAll("#fuel-log-form input, #fuel-log-form select");
-      inputFields.forEach((input, index) => {
-        const cell = newRow.insertCell(index);
-        cell.textContent = input.value;
-      });
-      // Clear the input fields after submission
-      inputFields.forEach((input) => {
-        input.value = "";
-      });
+      handleFormSubmission();
     });
-  });  
+  
+    // Handle form submission when pressing the "Enter" key
+    dateInput.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleFormSubmission();
+      }
+    });
+  
+    function handleFormSubmission() {
+      // Check if all required fields are filled
+      if (isFormValid()) {
+        // Implement the logic to handle form submission here
+        // For simplicity, we'll just add a new row to the table with the form data
+        const tableBody = document.querySelector("#fuel-log-table tbody");
+        const newRow = tableBody.insertRow();
+        const inputFields = document.querySelectorAll("#fuel-log-form input, #fuel-log-form select");
+        inputFields.forEach((input, index) => {
+          const cell = newRow.insertCell(index);
+          cell.textContent = input.value;
+        });
+        // Clear the input fields after submission
+        inputFields.forEach((input) => {
+          input.value = "";
+        });
+      } else {
+        alert("Please fill in all required fields.");
+      }
+    }
+  
+    function isFormValid() {
+      const inputFields = document.querySelectorAll("#fuel-log-form input, #fuel-log-form select");
+      for (const input of inputFields) {
+        if (input.hasAttribute("required") && !input.value) {
+          return false;
+        }
+      }
+      return true;
+    }
+  });
+  
